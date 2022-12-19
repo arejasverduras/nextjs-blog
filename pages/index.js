0967b2d0b5e216ea7e20dc761c4127d3b13cd17c
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-19 18:38:11 +0100

    update header

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h1>PrevTEST</h1>
+        <h1>MiBlog</h1>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
